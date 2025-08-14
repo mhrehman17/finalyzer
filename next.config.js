c514f5451b49a498afe63225c115cdffd55c00aa
LEADS <mhrehman@ieee.org>
2025-08-14 16:44:06 +0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // add any other Next.js config options you had in next.config.ts
+}
+
+module.exports = nextConfig;
